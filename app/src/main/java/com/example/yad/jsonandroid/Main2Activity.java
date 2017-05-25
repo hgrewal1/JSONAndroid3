79c4ac059f24463c605481c47f754ef5e0bdd7b2
hgrewal1 <harvinder.grewal@hotmail.com>
2017-05-25 00:40:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yad/jsonandroid/Main2Activity.java
@@ -0,0 +1,122 @@
+package com.example.yad.jsonandroid;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
+public class Main2Activity extends AppCompatActivity {
+    TextView out1, out2, out3, out4,out5,out6,out7,out8;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        out1 = (TextView) findViewById(R.id.textView);
+        out2 = (TextView) findViewById(R.id.textView2);
+        out3 = (TextView) findViewById(R.id.textView3);
+        out4 = (TextView) findViewById(R.id.textView4);
+        out5 = (TextView) findViewById(R.id.textView5);
+        out6 = (TextView) findViewById(R.id.textView6);
+        out7 = (TextView) findViewById(R.id.textView7);
+        out8 = (TextView) findViewById(R.id.textView8);
+        new MyTask().execute();
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+        String o1, o2, o3, o4,o5,o6,o7,o8;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            URL url = null;
+            Intent mynewintent = getIntent();
+            String r = mynewintent.getStringExtra("LON");
+            String s = mynewintent.getStringExtra("LAT");
+
+            try {
+                url = new URL("http://144.217.163.57:8080/cegepgim/mobile/tutorials/login&"+r+"&"+s);
+
+                HttpURLConnection client = null;
+                client = (HttpURLConnection) url.openConnection();
+                client.setRequestMethod("GET");
+                int responseCode = client.getResponseCode();
+                System.out.println("\n Sending 'GET' request to url:" + url);
+                System.out.println("\n Response code:" + responseCode);
+                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+                BufferedReader in = new BufferedReader(myInput);
+                String inputline;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputline = in.readLine()) != null) {
+                    response.append(inputline);
+                }
+                in.close();
+                //print rsult
+                System.out.println(response.toString());
+                JSONObject obj = new JSONObject(response.toString());
+                o5 = obj.getString("Status");
+                if(o5.equals("ok")){
+                    o1 = obj.getString("UserId");
+                    o2 = obj.getString("FirstName");
+                    o3 = obj.getString("LastName");
+                    o4 = obj.getString("Email");}
+
+
+
+                else{
+
+
+
+                    o1 = obj.getString("UserId");
+                    o2 = obj.getString("Message");
+                    o4 = obj.getString("TimeStamp");
+
+
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+
+            out1.setText(o1);
+            out2.setText(o2);
+            out3.setText(o3);
+            out4.setText(o4);
+            out5.setText(o5);
+            out6.setText(o6);
+
+            super.onPostExecute(result);
+
+
+
+        }
+
+
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/yad/jsonandroid/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.yad.jsonandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void nextpage(View view){
+        EditText in1=(EditText) findViewById(R.id.editText) ;
+        EditText in2=(EditText) findViewById(R.id.editText2) ;
+        Intent MyIntent=new Intent(this,Main2Activity.class);
+        MyIntent.putExtra("LON",in2.getText().toString());
+        MyIntent.putExtra("LAT",in1.getText().toString());
+
+        startActivity(MyIntent);
+    }
+    public void next(View view){
+
+        Intent MyIntent=new Intent(this,json.class);
+
+        startActivity(MyIntent);
+    }
+
+
+
+}
